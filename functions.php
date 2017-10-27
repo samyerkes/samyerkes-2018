78d3eb562713ecc67c74f4db94fd87370836fbc4
Samuel W Yerkes <syerkes@vcu.edu>
2017-10-27 10:39:56 -0400

    Disabled emoji resources.

--- functions.php
+++ functions.php
@@ -3,3 +3,18 @@
 wp_enqueue_style( 'style', get_stylesheet_uri() );
 
 wp_enqueue_script( 'script', get_template_directory_uri() . '/js/app.js', array (), 1, true);
+
+/* Disable emojis */
+function disable_wp_emojicons() {
+  // all actions related to emojis
+  remove_action( 'admin_print_styles', 'print_emoji_styles' );
+  remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
+  remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+  remove_action( 'wp_print_styles', 'print_emoji_styles' );
+  remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
+  remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
+  remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
+  // filter to remove TinyMCE emojis
+  add_filter( 'tiny_mce_plugins', 'disable_emojicons_tinymce' );
+}
+add_action( 'init', 'disable_wp_emojicons' );
