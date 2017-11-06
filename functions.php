a4f058968c03ff386b73c1863c78937aa5001ede
Samuel W Yerkes <syerkes@vcu.edu>
2017-11-05 22:07:17 -0500

    Added async of js.

--- functions.php
+++ functions.php
@@ -15,18 +15,27 @@ function disable_wp_emojicons() {
 }
 add_action( 'init', 'disable_wp_emojicons' );
 
-function add_custom_styles_and_scripts() {
-  wp_enqueue_style( 'style', get_template_directory_uri() . '/dist/app.css' );  
-  wp_enqueue_script( 'script', get_template_directory_uri() . '/dist/app.js', array (), 1, true);
+// Async load scripts
+add_filter( 'script_loader_tag', 'samyerkes_async_scripts', 10, 3 );
+function samyerkes_async_scripts( $tag, $handle, $src ) {
+  // The handles of the enqueued scripts we want to async
+  if ( $handle == 'samyerkes' ) {
+    return '<script type="text/javascript" src="' . $src . '" async></script>' . "\n";
+  }
+  return $tag;
+} 
+
+function samyerkes_add_custom_styles_and_scripts() {
+  wp_enqueue_style( 'samyerkes', get_template_directory_uri() . '/dist/app.css' );  
+  wp_enqueue_script( 'samyerkes', get_template_directory_uri() . '/dist/app.js', array (), 1, true);
 }
-add_action( 'wp_enqueue_scripts', 'add_custom_styles_and_scripts' );
+add_action( 'wp_enqueue_scripts', 'samyerkes_add_custom_styles_and_scripts' );
 
-add_action( 'wp_head', 'inc_manifest_link' );
-
-// Creates the link tag
-function inc_manifest_link() {   
+// Creates the manifest link tag
+function samyerkes_manifest_link() {   
   echo '<link rel="manifest" href="' . get_template_directory_uri(). '/manifest.json">';
 }
+add_action( 'wp_head', 'samyerkes_manifest_link' );
 
 // Remove Wordpress Generator (version)
 remove_action('wp_head', 'wp_generator');
@@ -34,8 +43,8 @@ remove_action('wp_head', 'wp_generator');
 add_theme_support( 'post-thumbnails', array( 'post', 'photos' ) );
 
 // Custom Image Gallery Markup
-add_filter('post_gallery','customFormatGallery',10,2);
-function customFormatGallery($string,$attr){
+add_filter('post_gallery','samyerkes_custom_image_gallery_format',10,2);
+function samyerkes_custom_image_gallery_format($string,$attr){
     $output = "<div class=\"columns is-multiline\">";
     $posts = get_posts(array('include' => $attr['ids'],'post_type' => 'attachment'));
     foreach($posts as $imagePost){
@@ -47,7 +56,7 @@ function customFormatGallery($string,$attr){
     return $output;
 }
 
-function my_deregister_scripts(){
+function samyerkes_deregister_scripts(){
   wp_deregister_script( 'wp-embed' );
 }
-add_action( 'wp_footer', 'my_deregister_scripts' );
+add_action( 'wp_footer', 'samyerkes_deregister_scripts' );
