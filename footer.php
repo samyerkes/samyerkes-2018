9b2ec73b51593cf59ce2d1b5a602ee15f8ad1446
Sam Yerkes <samuelyerkes@gmail.com>
2019-07-04 03:29:17 +0000

    Removed status link in footer.

--- footer.php
+++ footer.php
@@ -4,9 +4,6 @@
                     <div class="column">
                         <p><strong>Pronounciation help:</strong> Yerkes rhymes with circus</p>
                     </div>
-                    <div class="column">
-                        <a id="system_status" href="https://status.samyerkes.com" class="button is-small is-outlined is-pulled-right is-success" target="_blank" rel="noopener">All systems are operational</a>
-                    </div>
                 </div>
             </div>
         </footer>
