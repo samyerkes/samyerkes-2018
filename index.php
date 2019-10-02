c761897c8ba3056544db542aa4dd80ab0b820d4d
Sam Yerkes <syerkes@vcu.edu>
2019-10-02 07:21:18 -0400

    Fixed flex container issue.

--- index.php
+++ index.php
@@ -17,34 +17,31 @@ get_header(); ?>
 	<div id="primary" class="content-area">
 		<main id="main-content" class="section">
 
-			<div class="container mx-auto py-16">
-				<div class="content ">
-					<div class="flex ">
-						
-						<div class="w-3/4 sm:w-full md:w-full lg:w-3/4 xl:w-3/4">
-							<?php if ( have_posts() ) : ?>
-								<?php while ( have_posts() ) : the_post(); ?>
-									<article>
-										<?php
-											if ( is_singular() ) :
-												include('templates/post.php');
-											else :
-												include('templates/post_card_excerpt.php');
-											endif;
-										?>
-									</article>
-								<?php endwhile; ?> 
-							<?php else : ?>
-								<h2>No posts to show...</h2>
-							<?php endif; ?>
+			<div class="container mx-auto py-16">			
+				<div class="flex flex-row flex-wrap">	
+					<div class="w-full lg:w-3/4 md:w-3/5">
+						<?php if ( have_posts() ) : ?>
+							<?php while ( have_posts() ) : the_post(); ?>
+								<article>
+									<?php
+										if ( is_singular() ) :
+											include('templates/post.php');
+										else :
+											include('templates/post_card_excerpt.php');
+										endif;
+									?>
+								</article>
+							<?php endwhile; ?> 
+						<?php else : ?>
+							<h2>No posts to show...</h2>
+						<?php endif; ?>
+					</div>
+					<div class="opacity-50 p-16 w-full lg:w-1/4 md:w-2/5">
+						<?php get_sidebar(); ?>
+						<?php include('templates/pagination.php'); ?>
+					</div>
+				</div>
 
-						</div>
-						<div class="w-auto flex-none opacity-50 p-16 max-w-xs">
-							<?php get_sidebar(); ?>
-							<?php include('templates/pagination.php'); ?>
-						</div>
-					</div>	
-				</div>	
 			</div>
 		</main>
 	</div>
