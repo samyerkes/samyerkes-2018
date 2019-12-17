bc308d29764859071124e6fcfac4506bbee316bd
Sam Yerkes <syerkes@vcu.edu>
2019-12-16 20:17:26 -0500

    Added health check badge.

--- footer.php
+++ footer.php
@@ -1,11 +1,12 @@
         <footer id="main-footer" class="container mx-auto border-t mt-8 pt-16 pb-12 text-gray-600 text-base">
-            <div class="container">
-                <div class="columns">
-                    <div class="column">
+            <div class="flex">
+                    <div class="flex-1">
                         <p class="pb-2 m-0 text-gray-500"><strong>Pronounciation help</strong></p>
                         <p>Yerkes (YER-kiss), rhymes with circus</p>
                     </div>
-                </div>
+                    <div class="flex-grow-0">
+                        <img src="https://health.samyerkes.com/badge/67ebdde2-30b5-475e-a3f4-a179a6d113e2/Sm-UKYrf/Websites.svg" />
+                    </div>
             </div>
         </footer>
 
